6c653c0eee34c93367450adf7b33c3092348daed
eaharon <eaharon@gmail.com>
2017-02-27 21:12:15 -0800

    add save and load functionality

--- app/pflMatrixApp.controller.js
+++ app/pflMatrixApp.controller.js
@@ -7,6 +7,7 @@ app.controller("pflMatrixController", function ($scope, pflMatrixService) {
 	$scope.diffOptions={};
 	$scope.diffOptRankings={};
 	$scope.topDiffOpts={}
+	$scope.userInfo={};
 	
 	$scope.showHelp=function(){
 		$( "#helpDialog" ).dialog({
@@ -38,6 +39,47 @@ app.controller("pflMatrixController", function ($scope, pflMatrixService) {
 		$scope.topDiffOpts=pflMatrixService.updateDifferentiationOptionsRanking(indicator.id, indicator.isSelected, $scope.diffOptRankings);
 	}	
 	
+	$scope.savePflMatrix=function(){
+		var indicatorList = [];
+		for (var pflInd in $scope.indicators) {
+			if ($scope.indicators[pflInd].isSelected) {
+				indicatorList.push(pflInd)
+			}
+		}
+		
+		var userDataForSave=angular.copy($scope.userInfo);
+		userDataForSave.indicators=indicatorList;
+		var dataStr=JSON.stringify(userDataForSave);
+		console.log(userDataForSave,dataStr);	
+		downloadFile('pflMatrix.pfl',dataStr);		
+	}
+	
+	document.getElementById('file-input').addEventListener('change', readSingleFile, false);
+	function readSingleFile(e) {
+		var file = e.target.files[0];
+		if (!file) {return;}
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			var contents = e.target.result;
+			console.log(contents);
+			loadMatrixFromFile(contents);
+		};
+		reader.readAsText(file);
+	}
+	
+	function downloadFile(filename, text) {
+		var element = document.createElement('a');
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+		element.setAttribute('download', filename);
+
+		element.style.display = 'none';
+		document.body.appendChild(element);
+
+		element.click();
+
+		document.body.removeChild(element);
+	}
+	
 	function loadMatrix(){
 		$scope.indicators=angular.copy(pflMatrixService.getIndicatorList());
 		$scope.diffOptions=angular.copy(pflMatrixService.getDifferentiationOptions());
@@ -45,11 +87,26 @@ app.controller("pflMatrixController", function ($scope, pflMatrixService) {
 		
 	}	
 	
+	function loadMatrixFromFile(fileStr){
+		$scope.userInfo = angular.copy(JSON.parse(fileStr));
+		$scope.userInfo.pflDate=new Date($scope.userInfo.pflDate);
+		console.log($scope.userInfo);					
+		
+		for (var pflInd in $scope.indicators) {$scope.indicators[pflInd].isSelected=false;}
+		
+		for(var i=0;i<$scope.userInfo.indicators.length;i++){
+			var pflInd=$scope.userInfo.indicators[i];
+			console.log($scope.indicators[pflInd]);
+			//$scope.updateRank(indicator)
+			$scope.indicators[pflInd].isSelected=true;
+			$scope.updateRank($scope.indicators[pflInd]);
+		}
+		$scope.$apply();
+		
+	}
+	
 	loadMatrix();
 	
-
-    
-
-
+  
 //end of controller
 });
